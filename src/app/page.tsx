6a670991187e0505a1fcb57001ef0b5494e0d59b
eric capiz <ericcapiz@gmail.com>
2024-02-20 12:50:42 -0600

    added homepage page type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-display text-5xl">YOOOOOO</h1>
-    </main>
-  );
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return <div className="text-red-500">Homepage</div>;
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
